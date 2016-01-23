59b14141d4388c10f1acf5509185c96e51825351
johnsona <adamjohnson35@gmail.com>
2016-01-23 12:40:34 -0500

    Initial commit.
    Have LEDs turning on, they are slightly dim and it is hard to tell yellow from white.

---
+++ start.py
@@ -0,0 +1,66 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+red = [37, 35, 33, 31]
+green = [26, 24, 22, 18]
+blue = [29, 23, 21, 19]
+cath = [8, 10, 12, 16]
+
+testMatrix = [[[True, True, True], [True, False, True], [False, False, True], [True, True, False]], [[False, True, False], [False, True, True], [False, False, True], [False, True, False]], [[True, False, False], [False, False, True], [False, False, True], [False, True, True]], [[False, True, False], [True, False, False], [True, False, False], [False, False, True]]]
+
+for x in red:
+	GPIO.setup(x, GPIO.OUT)
+for x in green:
+	GPIO.setup(x, GPIO.OUT)
+for x in blue:
+	GPIO.setup(x, GPIO.OUT)
+for x in cath:
+	GPIO.setup(x, GPIO.OUT)
+
+def off():
+	for x in red:
+		GPIO.output(x, False)
+	for x in green:
+		GPIO.output(x, False)
+	for x in blue:
+		GPIO.output(x, False)
+	for x in cath:
+		GPIO.output(x, True)
+			
+def cathodesUp():
+	for x in cath:
+		GPIO.output(x, True)
+
+# This function takes in a 3-dimensional array
+# rows, columns, and colors are the dimensions
+def handleColorsRow(colorMatrix, sleep):
+	while 1:
+		for row in range(0, len(colorMatrix)):
+			for column in range(0, len(colorMatrix[row])):
+				GPIO.output(cath[column], False)
+				GPIO.output(red[row], colorMatrix[row][column][0])
+				GPIO.output(green[row], colorMatrix[row][column][1])
+				GPIO.output(blue[row], colorMatrix[row][column][2])
+				time.sleep(sleep)
+				GPIO.output(cath[column], True)
+				GPIO.output(red[row], False)
+				GPIO.output(green[row], False)
+				GPIO.output(blue[row], False)
+
+def handleColorsColumn(colorMatrix, sleep):
+	while 1:
+		for row in range(0, len(colorMatrix)):
+			for column in range(0, len(colorMatrix[row])):
+				GPIO.output(cath[row], False)
+				GPIO.output(red[column], colorMatrix[row][column][0])
+				GPIO.output(green[column], colorMatrix[row][column][1])
+				GPIO.output(blue[column], colorMatrix[row][column][2])
+				time.sleep(sleep)
+				GPIO.output(cath[row], True)
+				GPIO.output(red[column], False)
+				GPIO.output(green[column], False)
+				GPIO.output(blue[column], False)
+handleColorsColumn(testMatrix, 0.0005)
+#off()
